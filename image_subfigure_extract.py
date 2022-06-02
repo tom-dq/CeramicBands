cf0606161a76be6d82f33281ab67aae33ec76ede
Tom Wilson <tom.wilson66@gmail.com>
2022-06-02 15:03:42 +1000

    wip

--- image_subfigure_extract.py
+++ image_subfigure_extract.py
@@ -9,15 +9,25 @@ BASE_IMAGE_PATH = r"C:\Users\Tom Wilson\Documents\CeramicBandData\pics\4A"
 BASE_PATH = pathlib.Path(BASE_IMAGE_PATH)
 OUT_CROP_DIR = "cropped"
 
-CROP_BOX = (100, 100, 200, 200)
+L, W = 1208, 141
+CROP_BOX = (L, 66, 2560-(0), 1440-66-322)
 
 def make_subfigs():
     existing_pngs = BASE_PATH.glob("*.png")
 
+    out_path_base = BASE_PATH / OUT_CROP_DIR
+    os.makedirs(out_path_base, exist_ok=True)
+
+
 def crop_one_image(base_image: pathlib.Path, out_image: pathlib.Path):
     im = Image.open(base_image)
 
-    im_cropped = im.crop()
+    im_cropped = im.crop(CROP_BOX)
+
+    im_cropped.save(out_image)
+
 
 if __name__ == "__main__":
-    crop_one_image(BASE_PATH / "Case-0425.png", None)
+    make_subfigs()
+    out_path = BASE_PATH / OUT_CROP_DIR / "AA-Case-0425.png"
+    crop_one_image(BASE_PATH / "Case-0425.png", out_path)
